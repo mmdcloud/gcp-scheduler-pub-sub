71a4d02038a40a07e0696e8b65fc72c839364e52
mmdcloud <madmaxcloudpractice@gmail.com>
2024-12-15 06:04:06 +0530

    update: changed github-connection-id

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "im-workspace" {
  source = "terraform-google-modules/bootstrap/google//modules/im_cloudbuild_workspace"
- version = "~> 7.0"
+ version = "10.0.0"
 
  project_id = "our-mediator-443812-i8"
  deployment_id = "gcp-scheduler"
  im_deployment_repo_uri = "https://github.com/mmdcloud/gcp-scheduler-pub-sub"
  im_deployment_ref = "main"
 
+ github_app_installation_id = "mmdcloud-connection"
 }
