5253ec16ac9fe3d1222d954c2eba10210ce5a9c4
mmdcloud <madmaxcloudpractice@gmail.com>
2024-12-23 17:44:16 +0530

    update: changed gcp project config

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-module "im-workspace" {
- source = "terraform-google-modules/bootstrap/google//modules/im_cloudbuild_workspace"
- version = "10.0.0"
+# module "im-workspace" {
+#   source  = "terraform-google-modules/bootstrap/google//modules/im_cloudbuild_workspace"
+#   version = "10.0.0"
 
- project_id = "our-mediator-443812-i8"
- deployment_id = "gcp-scheduler-new"
- im_deployment_repo_uri = "https://github.com/mmdcloud/gcp-scheduler-pub-sub"
- im_deployment_ref = "main"
-}
+#   project_id             = "nodal-talon-445602-m1"
+#   deployment_id          = "gcp-scheduler-new"
+#   im_deployment_repo_uri = "https://github.com/mmdcloud/gcp-scheduler-pub-sub"
+#   im_deployment_ref      = "main"
+# }
