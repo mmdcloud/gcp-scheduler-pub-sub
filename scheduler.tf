6986b2de680903300cd9199b97e6badc117bf629
mmdcloud <madmaxcloudpractice@gmail.com>
2025-05-28 10:47:56 +0530

    update: removed unnecessary code

--- scheduler.tf
+++
@@ -1,9 +0,0 @@
-resource "google_cloud_scheduler_job" "event-scheduler-job" {
-  name        = "event-scheduler-job"
-  description = "event-scheduler-job"
-  schedule    = "*/5 * * * *"
-  pubsub_target {
-    topic_name = google_pubsub_topic.event-scheduler-topic.id
-    data       = base64encode("Mohit !")
-  }
-}
